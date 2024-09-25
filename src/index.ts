88af7332eeee545ab460b70bc3f28a3e7ae32fd9
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-09-26 04:16:07 +0900

    feat: return random element from db support

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,22 @@ import { Elysia } from "elysia";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { haikusTable } from "./db/schema";
+import { sql } from "drizzle-orm";
 
 config({ path: ".env" });
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.HAIKUAPI_DB_URL!);
 export const db = drizzle(client);
 
 const app = new Elysia()
   .get("/", () => "Hi")
-  .get("/rand", () => "Hello Elysia")
+  .get("/rand", () =>
+    db
+      .select()
+      .from(haikusTable)
+      .orderBy(sql`RANDOM()`)
+      .limit(1),
+  )
   .listen(3000);
 
 console.log(
