c949ecd58c5a489e7126630b4494fbd8fd8e8288
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-09-26 03:04:35 +0900

    feat: add postgres db support and env files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { Elysia } from "elysia";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+config({ path: ".env" });
+const client = postgres(process.env.DATABASE_URL!);
+export const db = drizzle(client);
 
 const app = new Elysia()
   .get("/", () => "Hi")
