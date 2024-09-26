a0a2903ca19026652ce5f5772786f8a16606d1a3
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-09-26 10:25:10 +0900

    refact: return only haiku and source

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
 import { Elysia } from "elysia";
-import { config } from "dotenv";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import { haikusTable } from "./db/schema";
+import { haikusTable } from "@/src/db/schema";
 import { sql } from "drizzle-orm";
-
-config({ path: ".env" });
-const client = postgres(process.env.HAIKUAPI_DB_URL!);
-export const db = drizzle(client);
+import { db } from "@/src/db";
 
 const app = new Elysia()
   .get("/", () => "Hi")
-  .get("/rand", () =>
-    db
+  .get("/rand", async () => {
+    const result = await db
       .select()
       .from(haikusTable)
       .orderBy(sql`RANDOM()`)
-      .limit(1),
-  )
+      .limit(1);
+    const haiku = result[0];
+    return {
+      haiku: `${haiku.first}\n${haiku.mid}\n${haiku.last}`,
+      source: haiku.source,
+    };
+  })
   .listen(3000);
 
 console.log(
