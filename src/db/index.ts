1e4b9a68dc9aef5d334b84101553957beca33a56
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-10-22 01:46:24 +0900

    refact: ⚡️add health endpoint + refactor db module

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,9 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { haikusTable } from "./schema";
 
 config({ path: ".env" });
 const client = postgres(process.env.HAIKUAPI_DB_URL!);
 export const db = drizzle(client);
+export { haikusTable };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import { Elysia } from "elysia";
-import { haikusTable } from "@/src/db/schema";
+import { db, haikusTable } from "@/src/db";
 import { sql } from "drizzle-orm";
-import { db } from "@/src/db";
+import { Logestic } from "logestic";
 
 const app = new Elysia()
-  .get("/", () => "Hi")
-  .get("/rand", async () => {
+  .use(Logestic.preset("fancy"))
+  .onBeforeHandle(({ set }) => {
+    set.headers["x-powered-by"] = "Elysia";
+  })
+  .get("/_health", () => ({
+    status: "healthy",
+    uptime: process.uptime(),
+  }))
+  .get("/", ({ set }) => {
+    set.status = "Not Implemented"; // 501
+    return {
+      message: "Available endpoints: /random, /_health",
+      error: "Not Implemented",
+    };
+  })
+  .get("/random", async () => {
     const result = await db
       .select()
       .from(haikusTable)
@@ -13,7 +27,8 @@ const app = new Elysia()
       .limit(1);
     const haiku = result[0];
     return {
-      haiku: `${haiku.first}\n${haiku.mid}\n${haiku.last}`,
+      id: haiku.id,
+      haiku: [haiku.first, haiku.mid, haiku.last],
       source: haiku.source,
     };
   })
